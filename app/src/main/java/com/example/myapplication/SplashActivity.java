42f6f48c1a9722c4b17402fdd904b5aaa0981b08
Vali <valistoicabusteni@gmail.com>
2023-03-04 16:25:25 +0200

    Added Splah Screen

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Ascund Action Bar de pe Splah Screen
+        getSupportActionBar().hide();
+
+        // Handler pentru intent care asteapta 3 secunde
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,ProfileActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        },3000);
+    }
+}
