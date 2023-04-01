4b31c3696b2fee2b627854a90a6e4274c24662fd
Vali <valistoicabusteni@gmail.com>
2023-04-01 14:08:35 +0300

    Added functionality to the users fragment, added the ability to see all the registred users.

--- app/src/main/java/com/example/myapplication/UsersFragment.java
+++ app/src/main/java/com/example/myapplication/UsersFragment.java
@@ -2,63 +2,93 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link UsersFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class UsersFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView recyclerView;
+    AdapterUsers adapterUsers;
+    List<ModelUser> usersList;
 
     public UsersFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UsersFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UsersFragment newInstance(String param1, String param2) {
-        UsersFragment fragment = new UsersFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        // Init RecycleView
+        recyclerView = view.findViewById(R.id.users_recyclerView);
+        // Set it`s props
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Init user list
+        usersList = new ArrayList<>();
+
+        // getAll users
+        getAllUsers();
+
+        return view;
+    }
+
+    private void getAllUsers() {
+        // Get current user
+        FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Get path of database named "Users" containing users info
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+
+        // Get all data from path
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                usersList.clear();
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    ModelUser modelUser = ds.getValue(ModelUser.class);
+
+                    // Get all users except the currently singed in user
+                    if(!modelUser.getUid().equals(fUser.getUid())){
+                        usersList.add(modelUser);
+                    }
+
+                    // Adapter
+                    adapterUsers = new AdapterUsers(getActivity(), usersList);
+
+                    // Set adapter to recycler view
+                    recyclerView.setAdapter(adapterUsers);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
+
